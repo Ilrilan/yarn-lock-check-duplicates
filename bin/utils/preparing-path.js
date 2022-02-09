cda00fab22e82bb526f316045ea5cbf8b0595a72
r.a.rytikov <r.a.rytikov@tinkoff.ru>
2022-02-09 12:08:52 +0300

    fixes

--- bin/utils/preparing-path.js
+++ bin/utils/preparing-path.js
@@ -1,8 +1,9 @@
-const os = require('os')
+const os = require('os');
+const { join } = require('path');
 
 exports.preparingPath = (filePath) => {
     const pathToUnixPath = os.platform() === 'win32' ? (str) => str.replace(/\\/g, '/') : (str) => str
 
-    const currentPath = pathToUnixPath(process.cwd())
-    return currentPath + filePath
+    const currentPath = pathToUnixPath(process.cwd());
+    return join(currentPath, filePath)
 }
